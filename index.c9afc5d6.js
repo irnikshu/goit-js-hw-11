d499f8bc2997d87edba4fe68c42fc087314c1dfd
irnikshu <95768966+irnikshu@users.noreply.github.com>
2022-12-15 15:31:32 +0000

    Deploying to gh-pages from @ irnikshu/goit-js-hw-11@9456fb943fbddb8b248cad71d1ea0f3b47f90600 🚀

---
+++ index.c9afc5d6.js
@@ -0,0 +1,2 @@
+$((function(){var e=$(".header");$(window).scroll((function(){$(this).scrollTop()>1?e.addClass("header_fixed"):e.removeClass("header_fixed")}))}));
+//# sourceMappingURL=index.c9afc5d6.js.map
